f508546ebac7709ed0290805e596cbb4438029e9
minhnhat <minhnhat290120@gmail.com>
2022-01-28 11:39:15 +0700

    upload code

---
+++ .history/style_20220108171035.js
@@ -0,0 +1,15 @@
+// const bar = document.getElementById('bar')
+// const nav = document.getElementById('navbar')
+// const cl = document.getElementById('close')
+
+// if (bar) {
+//     bar.addEventListener('click', () => {
+//         nav.classList.add('active')
+//     })
+// }
+
+// if (cl) {
+//     cl.addEventListener('click', () => {
+//         cl.classList.remove('active')
+//     })
+// }
